c3a547f49ed4dcadf1ddc1d5c594ab21525b352e
thesinglecloud <hanfeng@zdns.cn>
2019-03-23 09:11:41 +0000

    fix log bug that when set timeout, stream will also be timeout

--- pkg/handler/pod_log.go
+++ pkg/handler/pod_log.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -11,12 +12,60 @@ import (
 
 	"github.com/zdnscloud/singlecloud/hack/sockjs"
 	"github.com/zdnscloud/singlecloud/pkg/logger"
+	"github.com/zdnscloud/singlecloud/pkg/types"
 )
 
 var (
 	MaxLineCountFromTail = int64(1000)
-	LogRequestTimeout    = 10 * time.Second
+	LogRequestTimeout    = 5 * time.Second
 )
+
+func (m *ClusterManager) openPodLog(cluster *types.Cluster, namespace, pod, container string) (io.ReadCloser, error) {
+	//when container has no log, Stream call will block forever
+	//if set client timeout, Stream will be timed out too
+	//so check whether there is any log first
+	oneline := int64(1)
+	podClient, _ := cluster.KubeClient.RestClientForObject(&corev1.Pod{}, LogRequestTimeout)
+	opts := corev1.PodLogOptions{
+		Follow:     false,
+		Container:  container,
+		Timestamps: false,
+		TailLines:  &oneline,
+	}
+	req := podClient.
+		Get().
+		Namespace(namespace).
+		Name(pod).
+		Resource("pods").
+		SubResource("log").
+		VersionedParams(&opts, scheme.ParameterCodec)
+	readCloser, err := req.Stream()
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, 8)
+	n, err := io.ReadFull(readCloser, buf)
+	readCloser.Close()
+	if n == 0 || err != nil {
+		return nil, io.EOF
+	}
+
+	podClient, _ = cluster.KubeClient.RestClientForObject(&corev1.Pod{}, 0)
+	opts = corev1.PodLogOptions{
+		Follow:     true,
+		Container:  container,
+		Timestamps: true,
+		TailLines:  &MaxLineCountFromTail,
+	}
+	req = podClient.
+		Get().
+		Namespace(namespace).
+		Name(pod).
+		Resource("pods").
+		SubResource("log").
+		VersionedParams(&opts, scheme.ParameterCodec)
+	return req.Stream()
+}
 
 func (m *ClusterManager) OpenPodLog(clusterID, namespace, pod, container string, r *http.Request, w http.ResponseWriter) {
 	cluster := m.get(clusterID)
@@ -26,23 +75,8 @@ func (m *ClusterManager) OpenPodLog(clusterID, namespace, pod, container string,
 	}
 
 	Sockjshandler := func(session sockjs.Session) {
-		podClient, _ := cluster.KubeClient.RestClientForObject(&corev1.Pod{}, LogRequestTimeout)
-		opts := corev1.PodLogOptions{
-			Follow:     true,
-			Container:  container,
-			Timestamps: true,
-			TailLines:  &MaxLineCountFromTail,
-		}
-		req := podClient.
-			Get().
-			Namespace(namespace).
-			Name(pod).
-			Resource("pods").
-			SubResource("log").
-			VersionedParams(&opts, scheme.ParameterCodec)
-		readCloser, err := req.Stream()
+		readCloser, err := m.openPodLog(cluster, namespace, pod, container)
 		if err != nil {
-			logger.Warn("request log get err %s", err.Error())
 			session.Close(503, err.Error())
 			return
 		}
