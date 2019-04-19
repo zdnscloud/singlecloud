8d5ba82ad97501ef23ab3c186518727b64f3fc38
thesinglecloud <hanfeng@zdns.cn>
2019-04-19 02:04:01 +0000

    agent add retry mechanism

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"time"
 
 	"github.com/zdnscloud/goproxy"
+	"github.com/zdnscloud/singlecloud/pkg/logger"
 )
 
 var (
@@ -14,15 +15,24 @@ var (
 	id      string
 )
 
+const (
+	RetryInterval = 10 * time.Second
+)
+
 func main() {
-	flag.StringVar(&addr, "addr", "", "single cloud server addr")
+	flag.StringVar(&addr, "addr", "127.0.0.1:80", "single cloud server addr")
 	flag.StringVar(&cluster, "cluster", "local", "cluster this agent belongs to")
 	flag.StringVar(&id, "id", "", "agent id with identify this agent")
 	flag.Parse()
 
+	logger.InitLogger()
+
 	url := fmt.Sprintf("ws://%s/apis/agent.zcloud.cn/v1/clusters/%s/register/%s", addr, cluster, id)
-	err := goproxy.RegisterAgent(url, func(string, string) bool { return true }, nil)
-	if err != nil {
-		log.Printf("agent exist with err: %s", err.Error())
+	for {
+		err := goproxy.RegisterAgent(url, func(string, string) bool { return true }, nil)
+		if err != nil {
+			logger.Warn("agent %s connect to cluster %s(%s) failed:%s, start to retry", id, cluster, addr, err.Error())
+		}
+		<-time.After(RetryInterval)
 	}
 }
