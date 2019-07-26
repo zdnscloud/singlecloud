133b56644bdb5d058d8d58b9d0528023a6f12298
hiwyw <wangyanwei@zdns.cn>
2019-07-26 19:20:19 +0800

    remove zke logconsole debug log

--- pkg/zke/logconsole.go
+++ pkg/zke/logconsole.go
@@ -42,7 +42,6 @@ func (c *Cluster) openLog(r *http.Request, w http.ResponseWriter) {
 		c.lock.Unlock()
 		go func() {
 			<-session.ClosedNotify()
-			log.Infof("zke logconsole handler recive session close signal")
 			close(done)
 		}()
 
@@ -61,7 +60,6 @@ func (c *Cluster) openLog(r *http.Request, w http.ResponseWriter) {
 		session.Close(503, "log is terminated")
 		c.lock.Unlock()
 		<-done
-		log.Infof("zke logconsole this handler func will exit")
 	}
 
 	path := fmt.Sprintf(WSZKELogPathTemp, c.Name)
