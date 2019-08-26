77e499c453d8f3d7c993c026ca15475aa708f6d8
joe-yu <yuhaijiao@zdns.cn>
2019-08-26 08:00:59 +0000

    change chart parent to namespace

--- pkg/types/chart.go
+++ pkg/types/chart.go
@@ -8,6 +8,7 @@ func SetChartSchema(schema *resttypes.Schema, handler resttypes.Handler) {
 	schema.Handler = handler
 	schema.CollectionMethods = []string{"GET"}
 	schema.ResourceMethods = []string{"GET"}
+	schema.Parents = []string{NamespaceType}
 }
 
 type Chart struct {
