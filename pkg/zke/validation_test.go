88b7563437464d68a34addc467fb8b2892937849
hiwyw <wangyanwei@zdns.cn>
2019-09-05 19:29:36 +0800

    add cluster config validate unit test

--- pkg/zke/validation_test.go
+++ pkg/zke/validation_test.go
@@ -10,43 +10,54 @@ import (
 func TestValidateDuplicateNodeName(t *testing.T) {
 	n1 := types.Node{
 		Name:    "master",
-		Address: "192.168.1.3",
+		Address: "192.168.1.1",
 	}
 
-	n2 = types.Node{
+	n2 := types.Node{
 		Name:    "master",
-		Address: "192."
+		Address: "192.168.1.2",
+	}
 
 	c := &types.Cluster{
-		Nodes: []types.Node{n1, n2, n},
+		Nodes: []types.Node{n1, n2},
 	}
 
 	ut.NotEqual(t, validateDuplicateNodes(c), nil)
 }
 
 func TestValidateDuplicateNodeIp(t *testing.T) {
-	n := types.Node{
-		Name:    "worker2",
-		Address: n1.Address,
-		Roles:   n2.Roles,
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.1",
 	}
 
 	c := &types.Cluster{
-		Nodes: []types.Node{n1, n2, n},
+		Nodes: []types.Node{n1, n2},
 	}
 
 	ut.NotEqual(t, validateDuplicateNodes(c), nil)
 }
 
 func TestValidateNoControlplane(t *testing.T) {
-	n := types.Node{
-		Name:    "worker2",
-		Address: "192.168.1.3",
-		Roles:   n2.Roles,
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleWorker, types.RoleEtcd},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleWorker, types.RoleEtcd},
 	}
 
 	c := &types.Cluster{
-		Nodes: []types.Node{n2, n},
+		Nodes: []types.Node{n1, n2},
 	}
 
 	ut.NotEqual(t, validateNodeCount(c), nil)
@@ -54,18 +65,141 @@ func TestValidateNoControlplane(t *testing.T) {
 
 func TestValidateNoEtcd(t *testing.T) {
 	n1 := types.Node{
-		Name:    "worker2",
-		Address: "192.168.1.3",
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleControlPlane},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
 		Roles:   []types.NodeRole{types.RoleWorker},
 	}
 
-	n2 :=
-
-
 	c := &types.Cluster{
-		Nodes: []types.Node{n2, n},
+		Nodes: []types.Node{n1, n2},
 	}
 
 	ut.NotEqual(t, validateNodeCount(c), nil)
 }
 
+func TestValidateNoWorker(t *testing.T) {
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleControlPlane},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleControlPlane, types.RoleEtcd},
+	}
+
+	c := &types.Cluster{
+		Nodes: []types.Node{n1, n2},
+	}
+
+	ut.NotEqual(t, validateNodeCount(c), nil)
+}
+
+func TestValidateNodeRolesConflict(t *testing.T) {
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleControlPlane, types.RoleWorker, types.RoleEtcd},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleWorker},
+	}
+
+	c := &types.Cluster{
+		Nodes: []types.Node{n1, n2},
+	}
+
+	ut.NotEqual(t, validateNodeRoleConflict(c), nil)
+}
+
+func TestValidateScAddress(t *testing.T) {
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleControlPlane, types.RoleWorker, types.RoleEtcd},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleWorker},
+	}
+
+	c := &types.Cluster{
+		Nodes:              []types.Node{n1, n2},
+		SingleCloudAddress: "192.168.1.1:8088",
+	}
+
+	ut.NotEqual(t, validateScAddress(c), nil)
+}
+
+func TestValidateCannotDeleteNode(t *testing.T) {
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleControlPlane, types.RoleWorker, types.RoleEtcd},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleWorker},
+	}
+
+	n3 := types.Node{
+		Name:    "master2",
+		Address: "192.168.1.3",
+		Roles:   []types.NodeRole{types.RoleControlPlane, types.RoleEtcd},
+	}
+
+	c1 := &types.Cluster{
+		Nodes: []types.Node{n1, n2},
+	}
+
+	c2 := &types.Cluster{
+		Nodes: []types.Node{n2, n3},
+	}
+
+	ut.NotEqual(t, validateCannotDeleteNode(c1, c2), nil)
+}
+
+func TestValidateNodesRoleChanage(t *testing.T) {
+	n1 := types.Node{
+		Name:    "master",
+		Address: "192.168.1.1",
+		Roles:   []types.NodeRole{types.RoleControlPlane, types.RoleWorker, types.RoleEtcd},
+	}
+
+	n2 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleWorker},
+	}
+
+	n3 := types.Node{
+		Name:    "worker",
+		Address: "192.168.1.2",
+		Roles:   []types.NodeRole{types.RoleWorker, types.RoleEdge},
+	}
+
+	c1 := &types.Cluster{
+		Nodes: []types.Node{n1, n2},
+	}
+
+	c2 := &types.Cluster{
+		Nodes: []types.Node{n1, n3},
+	}
+
+	ut.NotEqual(t, validateNodesRoleChanage(c1, c2), nil)
+}
