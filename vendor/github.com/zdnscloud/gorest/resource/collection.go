3f1bc7e0520b1859ffaeab3d50f8fd8b79c134f4
thesinglecloud <hanfeng@zdns.cn>
2019-09-17 06:34:52 +0000

    update gorest lib

--- vendor/github.com/zdnscloud/gorest/resource/collection.go
+++ vendor/github.com/zdnscloud/gorest/resource/collection.go
@@ -15,35 +15,68 @@ type ResourceCollection struct {
 }
 
 func NewResourceCollection(collection Resource, i interface{}) (*ResourceCollection, error) {
-	var resources []Resource
 	typ := collection.GetType()
-	if i != nil {
-		v := reflect.ValueOf(i)
-		if v.Kind() != reflect.Slice {
-			return nil, fmt.Errorf("list handler doesn't return slice but %v", v.Kind())
-		}
+	rs, err := interfaceToResourceCollection(typ, i)
+	if err != nil {
+		return nil, err
+	} else {
+		return &ResourceCollection{
+			Type:         "collection",
+			ResourceType: typ,
+			Resources:    rs,
+			collection:   collection,
+		}, nil
+	}
+}
 
-		l := v.Len()
-		if l > 0 {
-			resources := make([]Resource, 0, l)
-			e := v.Index(0)
-			if r, ok := e.Interface().(Resource); ok == false {
-				return nil, fmt.Errorf("list handler doesn't return slice of resource but %v", e.Kind())
-			} else if r.GetID() == "" {
-				return nil, fmt.Errorf("list handler get resource doesn't have id")
-			} else {
-				r.SetType(typ)
-				resources = append(resources, r)
-			}
+func interfaceToResourceCollection(typ string, i interface{}) ([]Resource, error) {
+	if i == nil {
+		return nil, nil
+	}
+
+	v := reflect.ValueOf(i)
+	if v.Kind() != reflect.Slice {
+		return nil, fmt.Errorf("list handler doesn't return slice but %v", v.Kind())
+	}
+	l := v.Len()
+	if l == 0 {
+		return nil, nil
+	}
+
+	resources := make([]Resource, 0, l)
+	for i := 0; i < l; i++ {
+		if r, err := valueToResource(typ, v.Index(i)); err != nil {
+			return nil, err
+		} else {
+			r.SetType(typ)
+			resources = append(resources, r)
 		}
 	}
 
-	return &ResourceCollection{
-		Type:         "collection",
-		ResourceType: typ,
-		Resources:    resources,
-		collection:   collection,
-	}, nil
+	return resources, nil
+}
+
+func valueToResource(typ string, e reflect.Value) (Resource, error) {
+	if e.Kind() != reflect.Ptr {
+		return nil, fmt.Errorf("resource isn't pointer but %v", e.Kind())
+	}
+
+	if e.IsNil() {
+		return nil, fmt.Errorf("resource is nil")
+	}
+
+	if rk, ok := e.Elem().Interface().(ResourceKind); ok == false {
+		return nil, fmt.Errorf("resource isn't a pointer to ResourceKind but %v", e)
+	} else if DefaultKindName(rk) != typ {
+		return nil, fmt.Errorf("resource with kind %v isn't same with the collection %v ", DefaultKindName(rk), typ)
+	}
+
+	r, ok := e.Interface().(Resource)
+	if ok == false {
+		return nil, fmt.Errorf("resource %v doesn't implement Resource interface", e.Kind())
+	}
+
+	return r, nil
 }
 
 func (rc *ResourceCollection) SetLinks(links map[ResourceLinkType]ResourceLink) {
