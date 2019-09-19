c042ceec37fff8b62d8cf368053d2feb069c133b
hiwyw <wangyanwei@zdns.cn>
2019-09-19 11:44:58 +0800

    remove minLen and maxLen rest tag for Registry IngressDomain field

--- pkg/types/registry.go
+++ pkg/types/registry.go
@@ -7,7 +7,7 @@ import (
 type Registry struct {
 	resource.ResourceBase `json:",inline"`
 	Cluster               string `json:"cluster" rest:"required=true,minLen=1,maxLen=128"`
-	IngressDomain         string `json:"ingressDomain" rest:"minLen=1,maxLen=128"`
+	IngressDomain         string `json:"ingressDomain"`
 	StorageClass          string `json:"storageClass"`
 	StorageSize           int    `json:"storageSize"`
 	AdminPassword         string `json:"adminPassword"`
