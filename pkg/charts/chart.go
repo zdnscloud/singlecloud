ca198517147b8e924b20f236b86a049d586dc5a2
joe-yu <yuhaijiao@zdns.cn>
2019-10-15 11:17:40 +0000

    add new chart postgres update redis config

--- pkg/charts/chart.go
+++ pkg/charts/chart.go
@@ -1,3 +1,3 @@
 package charts
 
-var SupportChartsConfig = []interface{}{Redis{}, Vanguard{}, Prometheus{}, Harbor{}}
+var SupportChartsConfig = []interface{}{Redis{}, Vanguard{}, Prometheus{}, Harbor{}, Postgresql{}}
