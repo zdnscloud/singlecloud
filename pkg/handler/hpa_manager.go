30a89cb8c7eac723bb1c2a717bf8b99405bf98d9
joe-yu <yuhaijiao@zdns.cn>
2019-11-12 11:06:59 +0000

    add custom metric

--- pkg/handler/hpa_manager.go
+++ pkg/handler/hpa_manager.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	asv1 "k8s.io/api/autoscaling/v1"
+	asv2beta2 "k8s.io/api/autoscaling/v2beta2"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	apiresource "k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8stypes "k8s.io/apimachinery/pkg/types"
 
@@ -34,7 +35,7 @@ func (m *HorizontalPodAutoscalerManager) Create(ctx *resource.Context) (resource
 
 	namespace := ctx.Resource.GetParent().GetID()
 	hpa := ctx.Resource.(*types.HorizontalPodAutoscaler)
-	if err := createHorizontalPodAutoscaler(cluster.KubeClient, namespace, hpa); err != nil {
+	if err := createHPA(cluster.KubeClient, namespace, hpa); err != nil {
 		if apierrors.IsAlreadyExists(err) {
 			return nil, resterror.NewAPIError(resterror.DuplicateResource,
 				fmt.Sprintf("duplicate horizontalpodautoscaler name %s", hpa.Name))
@@ -48,27 +49,115 @@ func (m *HorizontalPodAutoscalerManager) Create(ctx *resource.Context) (resource
 	return hpa, nil
 }
 
-func createHorizontalPodAutoscaler(cli client.Client, namespace string, hpa *types.HorizontalPodAutoscaler) error {
-	minReplicas := int32(hpa.MinReplicas)
-	cpuUtilizationPercentage := int32(hpa.CPUUtilizationPercentage)
-	k8sHpa := &asv1.HorizontalPodAutoscaler{
+func createHPA(cli client.Client, namespace string, hpa *types.HorizontalPodAutoscaler) error {
+	k8sHpaSpec, err := scHPAToK8sHPASpec(hpa)
+	if err != nil {
+		return err
+	}
+
+	k8sHpa := &asv2beta2.HorizontalPodAutoscaler{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      hpa.Name,
 			Namespace: namespace,
 		},
-		Spec: asv1.HorizontalPodAutoscalerSpec{
-			MinReplicas:                    &minReplicas,
-			MaxReplicas:                    int32(hpa.MaxReplicas),
-			TargetCPUUtilizationPercentage: &cpuUtilizationPercentage,
-			ScaleTargetRef: asv1.CrossVersionObjectReference{
-				APIVersion: workloadAPIVersion,
-				Kind:       hpa.ScaleTargetKind,
-				Name:       hpa.ScaleTargetName,
-			},
-		},
+		Spec: k8sHpaSpec,
 	}
 
 	return cli.Create(context.TODO(), k8sHpa)
+}
+
+func scHPAToK8sHPASpec(hpa *types.HorizontalPodAutoscaler) (asv2beta2.HorizontalPodAutoscalerSpec, error) {
+	var metrics []asv2beta2.MetricSpec
+	for _, metric := range hpa.Metrics {
+		metricSpec, err := scMetricSpecToK8sMetricSpec(metric)
+		if err != nil {
+			return asv2beta2.HorizontalPodAutoscalerSpec{}, err
+		}
+
+		metrics = append(metrics, metricSpec)
+	}
+
+	minReplicas := int32(hpa.MinReplicas)
+	return asv2beta2.HorizontalPodAutoscalerSpec{
+		MinReplicas: &minReplicas,
+		MaxReplicas: int32(hpa.MaxReplicas),
+		ScaleTargetRef: asv2beta2.CrossVersionObjectReference{
+			APIVersion: workloadAPIVersion,
+			Kind:       string(hpa.ScaleTargetKind),
+			Name:       hpa.ScaleTargetName,
+		},
+		Metrics: metrics,
+	}, nil
+}
+
+func scMetricSpecToK8sMetricSpec(metric types.MetricSpec) (asv2beta2.MetricSpec, error) {
+	target, err := scMetricValueToK8sMetricTarget(metric.Type, metric.TargetType, metric.MetricValue)
+	if err != nil {
+		return asv2beta2.MetricSpec{}, err
+	}
+
+	switch metric.Type {
+	case types.MetricSourceTypeResource:
+		name, err := scResourceNameToK8sResourceName(string(metric.ResourceName))
+		if err != nil {
+			return asv2beta2.MetricSpec{}, err
+		}
+
+		return asv2beta2.MetricSpec{
+			Type: asv2beta2.ResourceMetricSourceType,
+			Resource: &asv2beta2.ResourceMetricSource{
+				Name:   name,
+				Target: target,
+			},
+		}, nil
+	case types.MetricSourceTypePods:
+		return asv2beta2.MetricSpec{
+			Type: asv2beta2.PodsMetricSourceType,
+			Pods: &asv2beta2.PodsMetricSource{
+				Metric: asv2beta2.MetricIdentifier{
+					Name: metric.MetricName,
+				},
+				Target: target,
+			},
+		}, nil
+	default:
+		return asv2beta2.MetricSpec{}, fmt.Errorf("metric source type %s is unsupported", metric.Type)
+	}
+}
+
+func scMetricValueToK8sMetricTarget(sourceType types.MetricSourceType, targetType types.MetricTargetType, target types.MetricValue) (asv2beta2.MetricTarget, error) {
+	switch targetType {
+	case types.MetricTargetTypeUtilization:
+		if sourceType != types.MetricSourceTypeResource {
+			return asv2beta2.MetricTarget{}, fmt.Errorf("only metric source type resource can use utilization target type")
+		}
+
+		if target.AverageUtilization == 0 {
+			return asv2beta2.MetricTarget{}, fmt.Errorf("averageUtilization must not be empty when type is %s", targetType)
+		}
+
+		utilization := int32(target.AverageUtilization)
+		return asv2beta2.MetricTarget{
+			Type:               asv2beta2.UtilizationMetricType,
+			AverageUtilization: &utilization,
+		}, nil
+	case types.MetricTargetTypeAverageValue:
+		if target.AverageValue == "" {
+			return asv2beta2.MetricTarget{}, fmt.Errorf("averageValue must not be empty when type is %s", targetType)
+		}
+
+		averageValue, e := apiresource.ParseQuantity(target.AverageValue)
+		if e != nil {
+			return asv2beta2.MetricTarget{}, fmt.Errorf("parse metric averageValue failed: %s", e.Error())
+		}
+
+		return asv2beta2.MetricTarget{
+			Type:         asv2beta2.AverageValueMetricType,
+			AverageValue: &averageValue,
+		}, nil
+	default:
+		return asv2beta2.MetricTarget{}, fmt.Errorf("metric target type %s is unsupported", targetType)
+	}
 }
 
 func (m *HorizontalPodAutoscalerManager) List(ctx *resource.Context) interface{} {
@@ -78,7 +167,7 @@ func (m *HorizontalPodAutoscalerManager) List(ctx *resource.Context) interface{}
 	}
 
 	namespace := ctx.Resource.GetParent().GetID()
-	k8sHpas, err := getHorizontalPodAutoscalers(cluster.KubeClient, namespace)
+	k8sHpas, err := getHPAs(cluster.KubeClient, namespace)
 	if err != nil {
 		if apierrors.IsNotFound(err) == false {
 			log.Warnf("list horizontalpodautoscaler info failed:%s", err.Error())
@@ -94,44 +183,99 @@ func (m *HorizontalPodAutoscalerManager) List(ctx *resource.Context) interface{}
 	return hpas
 }
 
-func getHorizontalPodAutoscalers(cli client.Client, namespace string) (*asv1.HorizontalPodAutoscalerList, error) {
-	hpas := asv1.HorizontalPodAutoscalerList{}
+func getHPAs(cli client.Client, namespace string) (*asv2beta2.HorizontalPodAutoscalerList, error) {
+	hpas := asv2beta2.HorizontalPodAutoscalerList{}
 	err := cli.List(context.TODO(), &client.ListOptions{Namespace: namespace}, &hpas)
 	return &hpas, err
 }
 
-func k8sHpaToScHpa(k8sHpa *asv1.HorizontalPodAutoscaler) *types.HorizontalPodAutoscaler {
+func k8sHpaToScHpa(k8sHpa *asv2beta2.HorizontalPodAutoscaler) *types.HorizontalPodAutoscaler {
 	var minReplicas int
 	if k8sHpa.Spec.MinReplicas != nil {
 		minReplicas = int(*k8sHpa.Spec.MinReplicas)
 	}
 
-	var cpuUtilizationPercentage int
-	if k8sHpa.Spec.TargetCPUUtilizationPercentage != nil {
-		cpuUtilizationPercentage = int(*k8sHpa.Spec.TargetCPUUtilizationPercentage)
+	var metrics []types.MetricSpec
+	for _, k8sMetric := range k8sHpa.Spec.Metrics {
+		if k8sMetric.Type == asv2beta2.ObjectMetricSourceType {
+			continue
+		}
+
+		metricSpec := types.MetricSpec{
+			Type: types.MetricSourceType(k8sMetric.Type),
+		}
+
+		if k8sMetric.Type == asv2beta2.ResourceMetricSourceType && k8sMetric.Resource != nil {
+			metricSpec.ResourceName = types.ResourceName(k8sMetric.Resource.Name)
+			metricSpec.TargetType = types.MetricTargetType(k8sMetric.Resource.Target.Type)
+			metricSpec.MetricValue = k8sMetricValueToScMetricValule(k8sMetric.Resource.Target.AverageValue,
+				k8sMetric.Resource.Target.AverageUtilization)
+		} else if k8sMetric.Type == asv2beta2.PodsMetricSourceType && k8sMetric.Pods != nil {
+			metricSpec.MetricName = k8sMetric.Pods.Metric.Name
+			metricSpec.TargetType = types.MetricTargetType(k8sMetric.Pods.Target.Type)
+			metricSpec.MetricValue = k8sMetricValueToScMetricValule(k8sMetric.Pods.Target.AverageValue,
+				k8sMetric.Pods.Target.AverageUtilization)
+		}
+
+		metrics = append(metrics, metricSpec)
 	}
 
-	var currentCPUUtilizationPercentage int
-	if k8sHpa.Status.CurrentCPUUtilizationPercentage != nil {
-		currentCPUUtilizationPercentage = int(*k8sHpa.Status.CurrentCPUUtilizationPercentage)
+	var currentMetrics []types.MetricStatus
+	for _, k8sCurrent := range k8sHpa.Status.CurrentMetrics {
+		if k8sCurrent.Type == asv2beta2.ObjectMetricSourceType {
+			continue
+		}
+
+		currentMetric := types.MetricStatus{
+			Type: types.MetricSourceType(k8sCurrent.Type),
+		}
+
+		if k8sCurrent.Type == asv2beta2.ResourceMetricSourceType && k8sCurrent.Resource != nil {
+			currentMetric.ResourceName = types.ResourceName(k8sCurrent.Resource.Name)
+			currentMetric.MetricValue = k8sMetricValueToScMetricValule(k8sCurrent.Resource.Current.AverageValue,
+				k8sCurrent.Resource.Current.AverageUtilization)
+		} else if k8sCurrent.Type == asv2beta2.PodsMetricSourceType && k8sCurrent.Pods != nil {
+			currentMetric.MetricName = k8sCurrent.Pods.Metric.Name
+			currentMetric.MetricValue = k8sMetricValueToScMetricValule(k8sCurrent.Pods.Current.AverageValue,
+				k8sCurrent.Pods.Current.AverageUtilization)
+		}
+
+		currentMetrics = append(currentMetrics, currentMetric)
 	}
 
 	hpa := &types.HorizontalPodAutoscaler{
-		Name:                     k8sHpa.Name,
-		ScaleTargetKind:          k8sHpa.Spec.ScaleTargetRef.Kind,
-		ScaleTargetName:          k8sHpa.Spec.ScaleTargetRef.Name,
-		MaxReplicas:              int(k8sHpa.Spec.MaxReplicas),
-		MinReplicas:              minReplicas,
-		CPUUtilizationPercentage: cpuUtilizationPercentage,
+		Name:            k8sHpa.Name,
+		ScaleTargetKind: types.ScaleTargetKind(k8sHpa.Spec.ScaleTargetRef.Kind),
+		ScaleTargetName: k8sHpa.Spec.ScaleTargetRef.Name,
+		MaxReplicas:     int(k8sHpa.Spec.MaxReplicas),
+		MinReplicas:     minReplicas,
+		Metrics:         metrics,
 		Status: types.HorizontalPodAutoscalerStatus{
-			CurrentReplicas:                 int(k8sHpa.Status.CurrentReplicas),
-			DesiredReplicas:                 int(k8sHpa.Status.DesiredReplicas),
-			CurrentCPUUtilizationPercentage: currentCPUUtilizationPercentage,
+			CurrentReplicas: int(k8sHpa.Status.CurrentReplicas),
+			DesiredReplicas: int(k8sHpa.Status.DesiredReplicas),
+			CurrentMetrics:  currentMetrics,
 		},
 	}
 	hpa.SetID(k8sHpa.Name)
 	hpa.SetCreationTimestamp(k8sHpa.CreationTimestamp.Time)
 	return hpa
+}
+
+func k8sMetricValueToScMetricValule(k8sAverageValue *apiresource.Quantity, k8sAverageUtilization *int32) types.MetricValue {
+	var averageUtilization int
+	if k8sAverageUtilization != nil {
+		averageUtilization = int(*k8sAverageUtilization)
+	}
+
+	var averageValue string
+	if k8sAverageValue != nil {
+		averageValue = k8sAverageValue.String()
+	}
+
+	return types.MetricValue{
+		AverageUtilization: averageUtilization,
+		AverageValue:       averageValue,
+	}
 }
 
 func (m *HorizontalPodAutoscalerManager) Get(ctx *resource.Context) resource.Resource {
@@ -142,7 +286,7 @@ func (m *HorizontalPodAutoscalerManager) Get(ctx *resource.Context) resource.Res
 
 	namespace := ctx.Resource.GetParent().GetID()
 	hpa := ctx.Resource.(*types.HorizontalPodAutoscaler)
-	k8sHpa, err := getHorizontalPodAutoscaler(cluster.KubeClient, namespace, hpa.GetID())
+	k8sHpa, err := getHPA(cluster.KubeClient, namespace, hpa.GetID())
 	if err != nil {
 		if apierrors.IsNotFound(err) == false {
 			log.Warnf("get horizontalpodautoscaler info failed:%s", err.Error())
@@ -153,8 +297,8 @@ func (m *HorizontalPodAutoscalerManager) Get(ctx *resource.Context) resource.Res
 	return k8sHpaToScHpa(k8sHpa)
 }
 
-func getHorizontalPodAutoscaler(cli client.Client, namespace, name string) (*asv1.HorizontalPodAutoscaler, error) {
-	hpa := asv1.HorizontalPodAutoscaler{}
+func getHPA(cli client.Client, namespace, name string) (*asv2beta2.HorizontalPodAutoscaler, error) {
+	hpa := asv2beta2.HorizontalPodAutoscaler{}
 	err := cli.Get(context.TODO(), k8stypes.NamespacedName{namespace, name}, &hpa)
 	return &hpa, err
 }
@@ -167,7 +311,7 @@ func (m *HorizontalPodAutoscalerManager) Update(ctx *resource.Context) (resource
 
 	namespace := ctx.Resource.GetParent().GetID()
 	hpa := ctx.Resource.(*types.HorizontalPodAutoscaler)
-	k8sHpa, err := getHorizontalPodAutoscaler(cluster.KubeClient, namespace, hpa.GetID())
+	k8sHpa, err := getHPA(cluster.KubeClient, namespace, hpa.GetID())
 	if err != nil {
 		if apierrors.IsNotFound(err) == false {
 			return nil, resterror.NewAPIError(resterror.NotFound,
@@ -178,8 +322,7 @@ func (m *HorizontalPodAutoscalerManager) Update(ctx *resource.Context) (resource
 		}
 	}
 
-	resetHorizontalPodAutoscaler(k8sHpa, hpa)
-	if err := cluster.KubeClient.Update(context.TODO(), k8sHpa); err != nil {
+	if err := updateHPA(cluster.KubeClient, k8sHpa, hpa); err != nil {
 		return nil, resterror.NewAPIError(types.ConnectClusterFailed,
 			fmt.Sprintf("update horizontalpodautoscaler failed %s", err.Error()))
 	}
@@ -187,12 +330,14 @@ func (m *HorizontalPodAutoscalerManager) Update(ctx *resource.Context) (resource
 	return hpa, nil
 }
 
-func resetHorizontalPodAutoscaler(k8sHpa *asv1.HorizontalPodAutoscaler, hpa *types.HorizontalPodAutoscaler) {
-	minReplicas := int32(hpa.MinReplicas)
-	cpuUtilizationPercentage := int32(hpa.CPUUtilizationPercentage)
-	k8sHpa.Spec.MinReplicas = &minReplicas
-	k8sHpa.Spec.MaxReplicas = int32(hpa.MaxReplicas)
-	k8sHpa.Spec.TargetCPUUtilizationPercentage = &cpuUtilizationPercentage
+func updateHPA(cli client.Client, k8sHpa *asv2beta2.HorizontalPodAutoscaler, hpa *types.HorizontalPodAutoscaler) error {
+	k8sHpaSpec, err := scHPAToK8sHPASpec(hpa)
+	if err != nil {
+		return err
+	}
+
+	k8sHpa.Spec = k8sHpaSpec
+	return cli.Update(context.TODO(), k8sHpa)
 }
 
 func (m *HorizontalPodAutoscalerManager) Delete(ctx *resource.Context) *resterror.APIError {
@@ -203,7 +348,7 @@ func (m *HorizontalPodAutoscalerManager) Delete(ctx *resource.Context) *resterro
 
 	namespace := ctx.Resource.GetParent().GetID()
 	hpa := ctx.Resource.(*types.HorizontalPodAutoscaler)
-	if err := deleteHorizontalPodAutoscaler(cluster.KubeClient, namespace, hpa.GetID()); err != nil {
+	if err := deleteHPA(cluster.KubeClient, namespace, hpa.GetID()); err != nil {
 		if apierrors.IsNotFound(err) == false {
 			return resterror.NewAPIError(resterror.NotFound,
 				fmt.Sprintf("horizontalpodautoscaler %s with namespace %s doesn't exist", hpa.GetID(), namespace))
@@ -216,8 +361,8 @@ func (m *HorizontalPodAutoscalerManager) Delete(ctx *resource.Context) *resterro
 	return nil
 }
 
-func deleteHorizontalPodAutoscaler(cli client.Client, namespace, name string) error {
-	hpa := &asv1.HorizontalPodAutoscaler{
+func deleteHPA(cli client.Client, namespace, name string) error {
+	hpa := &asv2beta2.HorizontalPodAutoscaler{
 		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
 	}
 	return cli.Delete(context.TODO(), hpa)
--- pkg/types/horizontalpodautoscaler.go
+++ pkg/types/horizontalpodautoscaler.go
@@ -4,23 +4,71 @@ import (
 	"github.com/zdnscloud/gorest/resource"
 )
 
+type ScaleTargetKind string
+
+const (
+	ScaleTargetKindDeployment  ScaleTargetKind = "deployment"
+	ScaleTargetKindStatefulSet ScaleTargetKind = "statefulset"
+)
+
+type MetricSourceType string
+
+const (
+	MetricSourceTypeResource = "Resource"
+	MetricSourceTypePods     = "Pods"
+)
+
+type ResourceName string
+
+const (
+	ResourceNameCPU    ResourceName = "cpu"
+	ResourceNameMemory ResourceName = "memory"
+)
+
+type MetricTargetType string
+
+const (
+	MetricTargetTypeUtilization  MetricTargetType = "Utilization"
+	MetricTargetTypeAverageValue MetricTargetType = "AverageValue"
+)
+
+type MetricSpec struct {
+	Type         MetricSourceType `json:"type,omitempty"`
+	MetricName   string           `json:"metricName,omitempty"`
+	ResourceName ResourceName     `json:"resourceName,omitempty"`
+	TargetType   MetricTargetType `json:"targetType,omitempty"`
+	MetricValue  `json:",inline"`
+}
+
+type MetricValue struct {
+	AverageValue       string `json:"averageValue,omitempty"`
+	AverageUtilization int    `json:"averageUtilization,omitempty"`
+}
+
+type HorizontalPodAutoscalerStatus struct {
+	CurrentReplicas int            `json:"currentReplicas,omitempty"`
+	DesiredReplicas int            `json:"desiredReplicas,omitempty"`
+	CurrentMetrics  []MetricStatus `json:"currentMetrics,omitempty"`
+}
+
+type MetricStatus struct {
+	Type         MetricSourceType `json:"type,omitempty"`
+	MetricName   string           `json:"metricName,omitempty"`
+	ResourceName ResourceName     `json:"resourceName,omitempty"`
+	MetricValue  `json:",inline"`
+}
+
 type HorizontalPodAutoscaler struct {
-	resource.ResourceBase    `json:",inline"`
-	Name                     string                        `json:"name"`
-	ScaleTargetKind          string                        `json:"scaleTargetKind" rest:"required=true,options=deployment|statefulset"`
-	ScaleTargetName          string                        `json:"scaleTargetName" rest:"required=true"`
-	MinReplicas              int                           `json:"minReplicas"`
-	MaxReplicas              int                           `json:"maxReplicas" rest:"required=true"`
-	CPUUtilizationPercentage int                           `json:"cpuUtilizationPercentage"`
-	Status                   HorizontalPodAutoscalerStatus `json:"status,omitempty"`
+	resource.ResourceBase `json:",inline"`
+	Name                  string                        `json:"name"`
+	ScaleTargetKind       ScaleTargetKind               `json:"scaleTargetKind" rest:"required=true,options=deployment|statefulset"`
+	ScaleTargetName       string                        `json:"scaleTargetName" rest:"required=true"`
+	MinReplicas           int                           `json:"minReplicas"`
+	MaxReplicas           int                           `json:"maxReplicas" rest:"required=true"`
+	Metrics               []MetricSpec                  `json:"metrics,omitempty"`
+	Status                HorizontalPodAutoscalerStatus `json:"status,omitempty"`
 }
 
 func (H HorizontalPodAutoscaler) GetParents() []resource.ResourceKind {
 	return []resource.ResourceKind{Namespace{}}
-}
-
-type HorizontalPodAutoscalerStatus struct {
-	CurrentReplicas                 int `json:"currentReplicas,omitempty"`
-	DesiredReplicas                 int `json:"desiredReplicas,omitempty"`
-	CurrentCPUUtilizationPercentage int `json:"currentCPUUtilizationPercentage,omitempty"`
 }
