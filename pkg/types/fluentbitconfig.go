e9c0ebd720845f37f29950ceb2461e35da980ec0
yuchunyun <yuchunyun@zdns.cn>
2019-11-19 11:22:03 +0800

    1: change FluentBitConfig parents to workload;2: remove collect all containers log from fluent-bit configmap

--- pkg/types/fluentbitconfig.go
+++ pkg/types/fluentbitconfig.go
@@ -6,50 +6,15 @@ import (
 
 type FluentBitConfig struct {
 	resource.ResourceBase `json:",inline"`
-	Name                  string       `json:"-"`
-	Workload              WorkloadConf `json:"workload" rest:"required=true"`
-	RegExp                string       `json:"regexp" rest:"required=true"`
-	Time_Key              string       `json:"timeKey,omitempty"`
-	Time_Format           string       `json:"timeFormat,omitempty"`
-}
-
-type WorkloadConf struct {
-	Namespace string `json:"namespace" rest:"required=true"`
-	Name      string `json:"name" rest:"required=true"`
-	Kind      string `json:"kind" rest:"required=true"`
-}
-
-type InstanceConf struct {
-	Include string
-	Input   InputConf
-	Filter  FilterConf
-	Output  OutputConf
-	Parser  ParserConf
-}
-
-type InputConf struct {
-	Tag  string
-	Path string
-	DB   string
-}
-
-type FilterConf struct {
-	Match  string
-	Parser string
-}
-
-type OutputConf struct {
-	Match           string
-	Logstash_Prefix string
-}
-
-type ParserConf struct {
-	Name        string
-	Regex       string
-	Time_Key    string
-	Time_Format string
+	Name                  string `json:"-"`
+	Namespace             string `json:"-"`
+	OwnerType             string `json:"-"`
+	OwnerName             string `json:"-"`
+	RegExp                string `json:"regexp" rest:"required=true"`
+	Time_Key              string `json:"timeKey,omitempty"`
+	Time_Format           string `json:"timeFormat,omitempty"`
 }
 
 func (e FluentBitConfig) GetParents() []resource.ResourceKind {
-	return []resource.ResourceKind{Cluster{}}
+	return []resource.ResourceKind{Deployment{}, DaemonSet{}, StatefulSet{}}
 }
